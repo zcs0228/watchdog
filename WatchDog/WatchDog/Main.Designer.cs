bf6f380610b4a180717a03479e8ebb79b57adeae
zcs0228 <zcs0228@yahoo.com>
2015-02-10 12:09:39 +0800

    start

---
+++ WatchDog/WatchDog/Main.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WatchDog
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.txtselectedFile = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnDectypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtselectedFile
+            // 
+            this.txtselectedFile.Location = new System.Drawing.Point(138, 52);
+            this.txtselectedFile.Name = "txtselectedFile";
+            this.txtselectedFile.Size = new System.Drawing.Size(207, 21);
+            this.txtselectedFile.TabIndex = 0;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(352, 52);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(48, 23);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "选择";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(138, 138);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "加密";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(138, 90);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.PasswordChar = '*';
+            this.txtCode.Size = new System.Drawing.Size(207, 21);
+            this.txtCode.TabIndex = 3;
+            // 
+            // btnDectypt
+            // 
+            this.btnDectypt.Location = new System.Drawing.Point(270, 138);
+            this.btnDectypt.Name = "btnDectypt";
+            this.btnDectypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDectypt.TabIndex = 4;
+            this.btnDectypt.Text = "解密";
+            this.btnDectypt.UseVisualStyleBackColor = true;
+            this.btnDectypt.Click += new System.EventHandler(this.btnDectypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(46, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "文件路径：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(46, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "密    码：";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 231);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDectypt);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtselectedFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtselectedFile;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnDectypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+
+    }
+}
+
---
+++ WatchDog/WatchDog/Main.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WatchDog
+{
+    public partial class Main : Form
+    {
+        private string _fileExtensionName = "";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "所有文件(*.*)|*.*";
+            fileDialog.RestoreDirectory = true;
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = fileDialog.FileName;
+                _fileExtensionName = Path.GetExtension(file);
+                this.txtselectedFile.Text = file;
+            }
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            string sourceFileName = "";
+            string code = "";
+            string savefileName = "";
+            if (this.txtselectedFile.Text.Trim() != "")
+            {
+                sourceFileName = this.txtselectedFile.Text.Trim();
+            }
+            else
+            {
+                MessageBox.Show("请选择加密文件！");
+                return;
+            }
+            if (this.txtCode.Text.Trim() != "")
+            {
+                code = this.txtCode.Text.Trim();
+            }
+            else
+            {
+                MessageBox.Show("请输入密码！");
+                return;
+            }
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "请选择保存路径";
+            saveFile.Filter = "所有文件(*.*)|*.*";
+            saveFile.RestoreDirectory = true;
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                savefileName = saveFile.FileName + _fileExtensionName;
+            }
+            else
+            {
+                return;
+            }
+            
+            try
+            {
+                DECFileEncryption.EncryptFile(sourceFileName, savefileName, code);
+                MessageBox.Show("加密成功！");
+                this.txtCode.Text = "";
+                this.txtselectedFile.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("加密失败！");
+            }
+        }
+
+        private void btnDectypt_Click(object sender, EventArgs e)
+        {
+            string sourceFileName = "";
+            string code = "";
+            string savefileName = "";
+            if (this.txtselectedFile.Text.Trim() != "")
+            {
+                sourceFileName = this.txtselectedFile.Text.Trim();
+            }
+            else
+            {
+                MessageBox.Show("请选择解密文件！");
+                return;
+            }
+            if (this.txtCode.Text.Trim() != "")
+            {
+                code = this.txtCode.Text.Trim();
+            }
+            else
+            {
+                MessageBox.Show("请输入密码！");
+                return;
+            }
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "请选择保存路径";
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                if (Path.GetExtension(saveFile.FileName) != _fileExtensionName)
+                {
+                    savefileName = saveFile.FileName + _fileExtensionName;
+                }
+                else
+                {
+                    savefileName = saveFile.FileName;
+                }
+            }
+            else
+            {
+                return;
+            }
+            
+            try
+            {
+                DECFileEncryption.DecryptFile(sourceFileName, savefileName, code);
+                MessageBox.Show("解密成功！");
+                this.txtselectedFile.Text = "";
+                this.txtCode.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("解密失败！");
+            }
+        }
+    }
+}
